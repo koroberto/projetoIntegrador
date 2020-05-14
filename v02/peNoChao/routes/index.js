5ea78aaa2cc072d26364ae1a187066490d82a1d7
krishnamurtir <kris.a.furtado@gmail.com>
2020-05-13 23:49:51 -0300

    index direcionado para login utilizando controller

--- v02/peNoChao/routes/index.js
+++ v02/peNoChao/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var LoginController = require('../controllers/LoginController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', LoginController.login);
+
+
+//Abaixo temos o codigo inicial criado pelo express generator
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
