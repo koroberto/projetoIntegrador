01528cb542a4f3c7ba6b35e658aae2e1c6be9897
krishnamurtir <kris.a.furtado@gmail.com>
2020-05-14 00:16:06 -0300

    cadastro com controller

--- v02/peNoChao/routes/index.js
+++ v02/peNoChao/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var LoginController = require('../controllers/LoginController');
+var CadastroController = require('../controllers/CadastroController');
 
 /* GET home page. */
 router.get('/', LoginController.login);
-
+router.get('/cadastro', CadastroController.cadastro);
 
 //Abaixo temos o codigo inicial criado pelo express generator
 // router.get('/', function(req, res, next) {
