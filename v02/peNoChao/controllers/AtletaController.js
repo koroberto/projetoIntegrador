d5f7f961eeca36a3a80cbc17a80fb7e502aa7aa1
krishnamurtir <kris.a.furtado@gmail.com>
2020-06-01 04:21:26 -0300

    botao publicar funcionando

--- v02/peNoChao/controllers/AtletaController.js
+++ v02/peNoChao/controllers/AtletaController.js
@@ -1,5 +1,28 @@
+const Sequelize = require('sequelize')
+const configs = require('../configs/database')
+const conect = new Sequelize(configs)
+/* Contantes acima add para conecta ao db */
+
+
 const AtletaController =  {
     view: (req,res) => {
+        return res.render('atleta');
+    },
+
+    //metodo para armazenar a publicação da resenha
+    store: async (req,res) => {
+        const {descricao} = req.body;
+        const publicar = await conect.query("INSERT INTO postagens (descricao)VALUES (:descricao)",
+        {
+            replacements:{
+                descricao,
+                jogadores_id: 2, //estou colocando ID fixo 2 para testar
+                create_at: new Date(),
+                update_at: new Date(),
+            },
+            type: Sequelize.QueryTypes.INSERT,
+        },);
+
         return res.render('atleta');
     }
 }
--- v02/peNoChao/routes/atleta.js
+++ v02/peNoChao/routes/atleta.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
 var AtletaController = require('../controllers/AtletaController');
 
 router.get('/', AtletaController.view);
+router.post('/', AtletaController.store);
 
 module.exports = router;
