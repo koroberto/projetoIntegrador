9024073ff521dc6f47923ce95e42a7c79699a321
krishnamurtir <kris.a.furtado@gmail.com>
2020-06-08 02:56:05 -0300

    cirado cadastroTimesController, rota no route index, e view copiada do cadastro jogador

---
+++ v02/peNoChao/controllers/CadastroTimesController.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const configs = require('../configs/database')
+const conect = new Sequelize(configs)
+
+
+const CadastroTimesController =  {
+    cadastro: (req,res) => {
+        return res.render('cadastroTimes');
+    },
+
+    store: async ( req, res ) =>{
+        const { nome,cep } = req.body
+        
+
+        const time = await conect.query("INSERT INTO times (nome,cep) VALUES (:nome, :cep)", 
+        {
+            replacements:{
+               nome, 
+               cep,
+             },
+            type: Sequelize.QueryTypes.INSERT,
+
+       }, );
+       if(!time){
+           return res.render('/cadastroTimes' , { msg:"erro ao cadastrar um usuario"})
+       }
+                
+                return res.render('atleta',{msg2:"Cadastro realizado com sucesso"});
+    },
+}
+
+module.exports = CadastroTimesController;
