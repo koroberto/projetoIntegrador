a2e41aa79b33a41d72d05cf75ae8da77e5396310
Diego Teodoro <46270019+digohbt@users.noreply.github.com>
2020-06-18 21:44:33 -0300

    Comentário: criado o fluxo para comentar e um post puxando todas informações necessárias , e gravando banco de dados

---
+++ v02/peNoChao/controllers/comentarioController.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const configs = require('../configs/database')
+const conect = new Sequelize(configs)
+
+const comentarioController = {
+    store: async (req, res) =>{
+        const {postagens_id,jogador_id, descricao} = req.body 
+        console.log(postagens_id,jogador_id, descricao)
+        const comentario = await conect.query("INSERT INTO comentarios(descricao, jogadores_id, postagens_id, create_at, update_at) VALUES(:descricao, :jogadores_id, :postagens_id, :create_at, :update_at)",
+        {
+            replacements:{
+                descricao, 
+                jogadores_id:jogador_id, 
+                postagens_id, 
+                create_at: new Date(),
+                update_at: new Date(),
+            },
+            type:Sequelize.QueryTypes.INSERT,
+
+        } );
+        if(comentario){
+            console.log(comentario)
+            return res.redirect('/atleta')
+        }
+    },
+}
+
+module.exports = comentarioController ; 
--- v02/peNoChao/routes/atleta.js
+++ v02/peNoChao/routes/atleta.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var AtletaController = require('../controllers/AtletaController');
+const comentarioController = require('../controllers/comentarioController');
 const auth = require ('../configs/auth.js')
 const upload = require('../configs/uploads')
 
 router.get('/',auth, AtletaController.view);
 router.post('/',upload.any(), AtletaController.store);
+
+router.post('/comentario',auth,comentarioController.store);
 
 module.exports = router;
