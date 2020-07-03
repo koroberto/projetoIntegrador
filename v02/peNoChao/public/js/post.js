c5e129571cc2af18e46903a999ad0e1bbb2937ae
Bruno Macena <macena@outlook.com>
2020-07-02 21:13:10 -0300

    Controle da exibição da div msgFoto

--- v02/peNoChao/public/js/post.js
+++ v02/peNoChao/public/js/post.js
@@ -1,10 +1,16 @@
-var postFile = document.querySelector(".postFile");
+const postFile = document.querySelector(".postFile");
 const btnPostar = document.getElementById("botaoPublicar");
 const msgFoto = document.getElementById("msgFoto");
 
 btnPostar.addEventListener("click", (event) => {
     if(postFile.files.length == 0){
         event.preventDefault();
-        msgFoto.classList.toggle("d-none"); 
+        msgFoto.classList.remove("d-none");
+    }
+})
+
+postFile.addEventListener("change", (event) => {
+    if(postFile.files.length != 0){
+        msgFoto.classList.add("d-none");
     }
 })
