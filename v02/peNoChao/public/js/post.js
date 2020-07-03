07ca72e1e9b0c0d9c0d78f801b746d8cc65f5bc9
Bruno Macena <macena@outlook.com>
2020-07-02 21:33:09 -0300

    Editando mensagem do postFile

--- v02/peNoChao/public/js/post.js
+++ v02/peNoChao/public/js/post.js
@@ -6,11 +6,15 @@ btnPostar.addEventListener("click", (event) => {
     if(postFile.files.length == 0){
         event.preventDefault();
         msgFoto.classList.remove("d-none");
+        msgFoto.textContent = "Carregue a imagem";
+        msgFoto.style.color = 'red';
     }
 })
 
 postFile.addEventListener("change", (event) => {
     if(postFile.files.length != 0){
-        msgFoto.classList.add("d-none");
+        // msgFoto.classList.add("d-none");
+        msgFoto.textContent = "Imagem carregada";
+        msgFoto.style.color = 'green';
     }
 })
